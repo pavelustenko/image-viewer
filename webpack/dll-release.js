a84c608ea063cf7b3831d195cff2f31196f8a473
wangcch <ci713@qq.com>
2019-09-12 17:56:00 +0800

    feat: transplant image-viewer
     from `meson-display`

--- webpack/dll-release.js
+++ webpack/dll-release.js
@@ -7,14 +7,14 @@ let DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-p
 let { matchCssRule, matchFontsRule } = require("./shared");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: ["react", "react-dom", "emotion", "immer", "dayjs", "lodash", "@jimengio/ruled-router"],
   output: {
     filename: "dll_vendors_[hash:8].js",
     path: path.join(__dirname, "../dist"),
     library: "dll_vendors_[hash:8]",
   },
-  devtool: "none",
+  devtool: "cheap-source-map",
   module: {
     rules: [matchCssRule, matchFontsRule],
   },
