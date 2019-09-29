7332e2bf61fb0316452b3ba382c5141a6e4e9318
ChenYong <chenyong@jimeng.io>
2019-09-29 18:21:33 +0800

    upgrade to new doc-frame

--- example/pages/demo-close-backdrop.tsx
+++ example/pages/demo-close-backdrop.tsx
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import { css, cx } from "emotion";
 
 import ImageViewer from "../../src/image-viewer";
+import { DocDemo } from "@jimengio/doc-frame";
 
 export default function DemoCloseBackdrop() {
   let [visible, setVisible] = useState(false);
 
   return (
     <div className={styleContainer}>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        Toggle
-      </button>
+      <DocDemo title="Demo of closing by dropdown" link="https://github.com/jimengio/image-viewer/blob/master/example/pages/demo-close-backdrop.tsx">
+        <button
+          onClick={() => {
+            setVisible(!visible);
+          }}
+        >
+          Show image
+        </button>
+      </DocDemo>
 
       <ImageViewer
         visible={visible}
--- example/pages/home.tsx
+++ example/pages/home.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { css } from "emotion";
 import { genRouter } from "controller/generated-router";
 import { HashLink } from "@jimengio/ruled-router/lib/dom";
+import { DocBlock } from "@jimengio/doc-frame";
 
 export default class Home extends React.Component {
   render() {
-    return <div>Home page</div>;
+    return (
+      <div>
+        <h3>Doc site for image viewer</h3>
+        <DocBlock embed content="Find more on [GitHub](https://github.com/jimengio/image-viewer)." />
+      </div>
+    );
   }
 }
