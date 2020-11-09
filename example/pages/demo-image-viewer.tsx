37584bf864d1af08d999a568d6cf211ce9a8738a
jiyinyiyong <jiyinyiyong@gmail.com>
2020-11-09 14:01:10 +0800

    replace personal domain

--- example/pages/demo-image-viewer.tsx
+++ example/pages/demo-image-viewer.tsx
@@ -22,7 +22,7 @@ export default function DemoImageViewer() {
       <ImageViewer
         visible={visible}
         // imageUrl={"http://192.168.1.180:8080/wallpaper/lamp.jpg"}
-        // imageUrl={"http://cdn.tiye.me/logo/jimeng-360x360.png"}
+        // imageUrl={"http://fe.jimu.io/logo/jimeng-360x360.png"}
         // imageUrl={"http://img1.iyiou.com/Editor/image/20160805/1470365447135769.jpg"}
         // imageUrl={"http://localhost:8080/test.png"}
         // imageUrl={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Proton_Zvezda_crop.jpg/300px-Proton_Zvezda_crop.jpg"}
